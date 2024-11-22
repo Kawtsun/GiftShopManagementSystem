80c74e09405a47059cac7bcc5a49e46700b20fe5
Kawtsun <morpheusjoshua71@gmail.com>
2024-11-22 23:01:21 +0800

    temp index

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Likhang Kultura - Welcome to our gift store!</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,100..900;1,100..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <div class="navbar">
+            <div class="logo">
+                <img src="images/logo.svg" alt="Likhang Kultura">
+            </div>
+            <div class="nav-center">
+                <nav class="navlinks">
+                    <ul>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="index.php">Catalog</a></li>
+                        <li><a href="index.php">About</a></li>
+                        <li><a href="index.php">Cart</a></li>
+                    </ul>
+                </nav>
+                <div class="buttons">
+                    <a href="index.php" class="login">Log In</a>
+                    <a href="index.php" class="signup">Sign Up</a>
+                </div>
+            </div>
+        </div>
+</header>
+</body>
+</html>
